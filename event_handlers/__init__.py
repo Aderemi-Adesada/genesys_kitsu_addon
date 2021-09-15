a38383ed4e74c48b184be896c298de5d22855da6
aderemi-adesada <adesadaaderemi@gmail.com>
2021-09-15 12:57:04 +0100

    update

--- event_handlers/__init__.py
+++ event_handlers/__init__.py
@@ -1,7 +1,20 @@
-from . import project_new, project_update
+from . import (
+    project_new,
+    project_update,
+    asset_update,
+    shot_update,
+    task_new,
+    task_assign,
+    task_unassign,
+    )
 
 event_map = {
     # "asset:new": event_log,
     "project:new": project_new,
     "project:update": project_update,
+    "asset:update": asset_update,
+    "shot:update": shot_update,
+    "task:new": task_new,
+    "task:assign": task_assign,
+    "task:unassign":task_unassign
 }
