e92e3d2d4c2e5017f5be7609a0e9e16e13f3b22b
aderemi-adesada <adesadaaderemi@gmail.com>
2021-09-15 17:48:09 +0100

    update

--- event_handlers/config.py
+++ event_handlers/config.py
@@ -1,5 +1,4 @@
 import os
-ENABLE_GENESIS = os.getenv("ENABLE_GENESIS", "False").lower() == "true"
 GENESIS_HOST = os.getenv("GENESIS_HOST", "http://127.0.0.1")
 GENESIS_PORT = os.getenv("GENESIS_PORT", 5002)
 SVN_SERVER_PARENT_URL = os.getenv("SVN_SERVER_PARENT_URL", "http://localhost/svn")
